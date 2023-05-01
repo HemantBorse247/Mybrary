cb7aeff0d05e1947291f72bafa2f443dbf96f794
hemantb24 <hemant.b2002@gmail.com>
2023-05-01 17:05:38 +0530

    created new routes for authors and learned about ejs and mongo models

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser') //* for getting and sending data from the body of the html pages easily (library)
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
@@ -21,5 +24,6 @@ db.once('open', () => console.log("connected to mongooose")) //* when connection
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
